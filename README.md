7f4a2964697bc2d2d672998fdfa889cdd1bd67bd
Antonio <parhamin2010@gmail.com>
2018-03-17 10:38:28 +0330

    first role

---
+++ README.md
@@ -0,0 +1,13 @@
+# JeyhoonPro
+ ===========================		
+ 	
+ Akharin Khabar project, comprising an basic MVC framework built in PHP.		
+	
+ Restrictions		
+ ============		
+ 	
+ Restrictions for this project are:		
+ <ol type="a">		
+ <li>Must be a blog platform</li>		
+ <li>Must use PHP</li>		
+ </ol>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('core/jeyhoon.php');
+require_once('core/controller.php');
+require_once('core/model.php');
+require_once('core/config.php');
+require_once('public/library/google/autoload.php');
+require_once('public/library/PHPMailer/class.phpmailer.php');
+require_once('public/library/PHPMailer/PHPMailerAutoload.php');
+require_once('public/library/mp3trim/phpmp3.php');
+require_once('public/library/MobileDetect/Mobile_Detect.php');
+require_once('public/library/rss/rss_feed.php');
+
+new Jeyhoon;
+?>
